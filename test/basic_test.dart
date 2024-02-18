64f8d1ef3e172e1cc351062f0bdbaaf167db78ba
Jhonacode <jhonacode2020@gmail.com>
2024-02-19 03:10:39 +1300

    Simple test

---
+++ test/basic_test.dart
@@ -0,0 +1,51 @@
+import 'package:test/test.dart';
+import 'package:todo_info/src/core/file_managment.dart';
+import 'package:todo_info/src/file_generator/generate_html.dart';
+import 'package:todo_info/src/file_generator/generate_md.dart';
+import 'package:todo_info/src/model/report_model.dart';
+import 'package:todo_info/todo_info.dart';
+
+void main() {
+  group('TODO Info Library Tests', () {
+
+    test('Generate Markdown report', () {
+
+      final mdFile = MDGenerator('test_report.md');
+      mdFile.reportList.add(Report(
+        priority: "low",
+        lineLocation: 20,
+        level: 1,
+        filePath: 'file.dart',
+        description: 'test_report',
+      ));
+      final inform = FileManagements(mdFile);
+
+      inform.build();
+
+      final reportContent = mdFile.exportTextFile();
+
+     expect(reportContent.contains("test_report"), true);
+
+    });
+
+    test('Generate HTML report', () {
+      final todo = TODO('Implement error handling', priority: TodoPriority.high);
+      final htmlFile = HTMLGenerator('test_report.html');
+      htmlFile.reportList.add(
+        Report(filePath: 'file path',
+            lineLocation: 30,
+            priority: todo.priority.name,
+            level: todo.priority.index,
+            description: todo.message,
+        )
+      );
+      final inform = FileManagements(htmlFile);
+
+      inform.build();
+
+      final reportContent = htmlFile.exportTextFile();
+
+      expect(reportContent.contains(todo.message), true);
+    });
+  });
+}
---
+++ test_report.md
@@ -0,0 +1,8 @@
+| ***File*** | ***Líne*** | ***Priority*** | ***Level*** |***Descriptión*** |
+|:---|:---:|:---|:---:|:---|
+| [./example/example_class.dart](./example/example_class.dart) | 22 | Critical | 5 | We need to create an external function
+| [./example/example_class.dart](./example/example_class.dart) | 16 | High | 4 | Need to implement this class, because it is important during build time
+| [./example/example_class.dart](./example/example_class.dart) | 19 | Medium | 3 | Requires async implementation
+| [./example/example_class.dart](./example/example_class.dart) | 30 | Normal | 2 | Evaluate if 'late' is required
+| [file.dart](file.dart) | 20 | Low | 1 | test_report
+| [./example/example_class.dart](./example/example_class.dart) | 26 | Low | 1 | It is recommended to do revisions
